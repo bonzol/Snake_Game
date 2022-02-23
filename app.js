ed0daefb51abe96f3a6d03bb6afea4794d3157a0
bonzol <bonzol@gmail.com>
2022-02-23 14:46:51 +0200

    change fail

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ var imgBombEx = new Image();
 imgBombEx.src = "img/BombEx.png";
 var endBomb = new Image();
 endBomb.src = "img/explosion.png";
-var imgFail2 = new Image();
-imgFail2.src = "img/fail2.png";
+var imgFail = new Image();
+imgFail.src = "img/fail.png";
 var imgHeadRight = new Image();
 imgHeadRight.src = "img/HeadRight.png";
 var imgHeadUp = new Image();
@@ -329,7 +329,6 @@ function buildSnake() {
         }
         else {
             let bodyNum = Math.floor(Math.random()*4)
-            console.log(bodyNum);
             ctx.drawImage(imgBody[bodyNum], p.x, p.y, snake.size.width, snake.size.height);
 
         }
@@ -422,7 +421,7 @@ function checkifEntwined() {
         setTemp.add(JSON.stringify(p));
     })
     if(setTemp.size != snake.place.length) {
-        ctx.drawImage(imgFail2, 0, 30, 400, 400);
+        ctx.drawImage(imgFail, 0, 30, 400, 400);
         failSound.play();
         drawGameOver(75);
         stopGame();
